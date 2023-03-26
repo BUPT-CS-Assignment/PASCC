36d3e1980b5d2d32d34a0d967dfc7c3af3f575a8
Jianxff <3109441270@qq.com>
2023-03-26 21:20:25 +0800

    [A] Add test file

---
+++ src/main.cc
@@ -0,0 +1,11 @@
+//
+// Created by jianxff on 2023/3/26.
+//
+
+#include "ast.h"
+#include "type.h"
+
+int main(){
+
+  return 0;
+}
---
+++ test/test.cc
@@ -0,0 +1,13 @@
+//
+// Created by jianxff on 2023/3/26.
+//
+
+#include "ast.h"
+#include "type.h"
+
+// for test
+
+int main(){
+
+  return 0;
+}
