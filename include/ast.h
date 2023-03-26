5f7b947e3d6497a9dce02b20a982ffa693d97b2b
Jianxff <3109441270@qq.com>
2023-03-26 16:10:25 +0800

    [A] AST classes update

---
+++ include/ast.h
@@ -0,0 +1,547 @@
+#ifndef PASCC_AST_H
+#define PASCC_AST_H
+
+#include <iostream>
+#include <vector>
+
+namespace ast{
+
+#define OUT(format, ...)                        \
+  do {                                          \
+    fprintf(stdout, format, ##__VA_ARGS__);     \  
+  } while (0);
+
+#define ERR(format, ...)                        \
+  do {                                          \
+    fprintf(stderr, format, ##__VA_ARGS__);     \  
+  } while (0);
+  
+#define FILE(file, format, ...)                 \
+  do {                                          \
+    fprintf(file, format, ##__VA_ARGS__);       \  
+  } while (0);
+
+
+
+
+class Node {
+ public:
+  Node();
+  ~Node();
+
+  void set_parent(Node* parent);
+  void append_child(Node* child);
+
+  Node* parent();
+  std::vector<Node*> child_list();
+
+  // type cast
+  template <typename T> T* StaticCast() {
+    return dynamic_cast<T*>(this);
+  }
+  
+  template <typename T> T* ReinterpretCast() {
+    return reinterpret_cast<T*>(this);
+  }
+
+
+  virtual void TransCode();
+  void TransCodeAt(int);
+  
+ protected:
+  // entry
+
+  Node* parent_;
+  std::vector<Node*> child_list_;
+ private:
+  
+};
+
+class LeafNode : public Node {
+ public:
+  void TransCode() override;
+ private:
+  
+};
+
+class ProgramNode : public Node {
+ //program → program_head | program_body
+ public:
+  void TransCode() override;
+  
+ private:
+};
+
+class ProgramHeadNode : public Node {
+ //programhead → program id (idlists)
+ public:
+  void TransCode() override;
+ private:
+
+};
+
+class ProgramBodyNode : public Node {
+  // program_body → const_declarations 
+  //                type_declarations
+  //                var_declarations
+  //                subprogram_declarations
+  //                compound_statement
+ public:
+  void TransCode() override;
+ private:
+
+};
+
+class IdListNode : public Node {
+ public:
+  enum class GrammarType {
+    SINGLE_ID,      //idlists → id
+    MULTIPLE_ID     //idlists → idlist,id 
+  };
+  void TransCode() override;
+  std::vector<Node*>* IdList() { return &child_list_; }
+ private:
+  GrammarType grammar_type_;
+};
+
+
+class ConstDeclarationsNode : public Node {
+ public:
+  enum class GrammarType {
+    EPSILON,             //const_declarations → EPSILON
+    CONST_DECLARATION    //const_declarations → const_declaration
+  };
+  void TransCode() override;
+ private:
+  GrammarType grammar_type_;
+};
+
+class ConstDeclarationNode : public Node {
+ public:
+  enum class GrammarType {
+    VALUE,        //ConstDeclaration → id = const_var
+    DECLARATION   //ConstDeclaration →ConstDeclaration; id = const_var
+  };
+  void TransCode() override;
+ private:
+  GrammarType grammar_type_;
+};
+
+class ConstValueNode : public Node {
+ public:
+  void TransCode() override;
+ private:
+};
+
+class VariableDeclarationsNode : public Node {
+ public:
+  enum class GrammarType {
+    EPSILON,          //VariableDeclarations→EPSILON
+    VAR_DECLARATION   //VariableDeclarations→var VariableDeclaration
+  };
+  void TransCode() override;
+ private:
+  GrammarType grammar_type_;
+};
+
+class VariableDeclarationNode : public Node {
+ public:
+  enum class GrammarType {
+    SINGLE_DECL,  //VariableDeclaration→idlist:type
+    MULTIPLE_DECL //VariableDeclaration→VariableDeclaration;idlist:type
+  };
+  void TransCode() override;
+ private:
+  GrammarType grammar_type_;
+};
+
+class TypeDeclarations : public Node {
+  public:
+    enum class GrammarType {
+      EPSILON,         //TypeDeclarations→EPSILON
+      TYPE_DECLARATION //TypeDeclarations→type TypeDeclaration
+  };
+  void TransCode() override;
+  private:
+    GrammarType grammar_type_;
+};
+
+class TypeDeclaration : public Node {
+ public:
+  enum class GrammarType {
+    SINGLE_DECL,  //TypeDeclaration→TypeDeclaration;id=type
+    MULTIPLE_DECL //id = type
+  };
+  void TransCode() override;
+ private:
+  GrammarType grammar_type_;
+
+};
+
+class TypeNode : public Node {
+ public:
+  enum class GrammarType {
+    BASIC_TYPE,
+    ARRAY,
+    RECORD_TYPE
+  };
+  void TransCode() override;
+  Node* get_basic_type();
+  GrammarType GetGrammarType() { return grammar_type_; }
+  void PeriodsTransCode();
+ private:
+  GrammarType grammar_type_;
+};
+
+class BasicTypeNode : public Node {
+ //BasicType →integer|real|boolean|char
+ public:
+  void TransCode() override;
+ private:
+};
+
+class RecordBodyNode : public Node {
+ public:
+  enum class GrammarType {
+    EPSILON,
+    VAR_DECLARATION
+  };
+  void TransCode() override;
+ private:
+  GrammarType grammar_type_;
+};
+
+class PeriodsNode : public Node {
+ public:
+  enum class GrammarType {
+    SINGLE_DECL,   //Periods→Period
+    MULTIPLE_DECL  //Periods→Periods,Period
+  };
+  void TransCode() override;
+ private:
+  GrammarType grammar_type_;
+};
+
+class PeriodNode : public Node {
+ //Period → const_var ... const var
+ public:
+  void TransCode() override;
+ private:
+  int len_;
+};
+
+class SubprogramDeclarationsNode : public Node {
+ public:
+  enum class GrammarType {
+    EPSILON,            //subprogram_declarations → EPSILON
+    SUBPROGRAM_DECL     //subprogram_declarations → subprogram_declarations subprogram ;        
+  };
+  void TransCode() override;
+ private:
+  GrammarType grammar_type_;
+};
+
+class SubprogramDeclarationNode : public Node {
+  //subprogram_declaration -> subprogram_head  program_body
+ public:
+  void TransCode() override;
+ private:
+};
+
+class SubprogramHeadNode : public Node {
+ public:
+  enum class GrammarType {
+    PROCEDURE,    // subprogram_head → procedure id formal_parameter
+    FUNCTION      // subprogram_head → function id formal_parameter : basic_type
+  };
+  void TransCode() override;
+
+ private:
+  GrammarType grammar_type_;
+};
+
+class FormalParamNode : public Node {
+ public:
+  enum class GrammarType {
+    EPSILON,      //formal_parameter → EPSILON 
+    PARAM_LISTS,  //formal_parameter → ( parameter_lists )
+  };
+  void TransCode() override;
+ private:
+  GrammarType grammar_type_;
+};
+
+class ParamListsNode : public Node {
+ public:
+  enum class GrammarType {
+    SINGLE_PARAM_LIST,      //param_lists→ param_list
+    MULTIPLE_PARAM_LIST,    //param_lists→ param_lists ; param_list
+  };
+  void TransCode() override;
+ private:
+  GrammarType grammar_type_;
+};
+
+class ParamListNode : public Node {
+ public:
+  enum class GrammarType {
+    VAR_PARAM,              //parameter_list → var_parameter
+    VALUE_PARAM,            //parameter_list → value_parameter 
+  };
+  void TransCode() override;
+ private:
+  GrammarType grammar_type_;
+};
+
+class VarParamNode : public Node {
+ //VarParam → var ValueParam
+ public:
+  void TransCode() override;
+ private:
+};
+
+class ValueParamNode : public Node {
+ //ValueParam → idlist : basic_type
+ public:
+  void TransCode() override;
+ private:
+
+};
+
+class CompoundStatementNode : public Node {
+ //CompoundStatement → begin StatementList end
+ public:
+  void TransCode() override;
+ private:
+
+};
+
+class StatementListNode : public Node {
+ public:
+  enum class GrammarType {
+    STATEMENT,                  //statement_list → statement 
+    STATEMENT_LIST_STATEMENT,   //statement_list → statement_list ; statement
+  };
+  void TransCode() override;
+ private:
+  GrammarType grammar_type_;
+};
+
+class StatementNode : public Node {
+ public:
+  enum class GrammarType {
+    EPSILON,              //statement → EPSILON
+    VAR_ASSIGN_OP_EXP,    //statement → variable assignop expression
+    FUNC_ASSIGN_OP_EXP,   //statement → func_id assignop expression
+    PROCEDURE_CALL,       //statement → procedure_call 
+    COMPOUND_STATEMENT,   //statement → compound_statement
+    IF_STATEMENT,         //statement → if expression then statement else_part
+    FOR_STATEMENT,        //statement → for id assignop expression downto expression do statement
+    READ_STATEMENT,       //statement → read ( variable_list )
+    WRITE_STATEMENT,      //statement → write ( expression_list )
+    CASE_STATEMET,        //statement → case expression of case_body end
+    WHILE_STATEMENT,      //statement → while expression do s
+    REPEAT_STATEMENT      //statement → repeat statement_list until expresssion
+  };
+  void TransCode() override;
+ private:
+  GrammarType grammar_type_;
+};
+
+class VariableListNode : public Node {
+ public:
+  enum class GrammarType {
+     VARIABLE,                  //variable_list → variable  
+     VARIABLE_LIST_VARIABLE,    //variable_list → variable_list , variable
+  };
+  void TransCode() override;
+ private:
+  GrammarType grammar_type_;
+
+};
+
+class VariableNode : public Node {
+  //variable → id id_varpart 
+ public:
+  void TransCode() override;
+ private:
+
+};
+
+class IDVarPartsNode : public Node {
+ public:
+  enum class GrammarType {
+    EPSILON,      //id_varparts → EPSILON 
+    MULTIPLE_IDv, //id_varparts → id_varparts id_varpart 
+  };
+  void TransCode() override;
+ private:
+  GrammarType grammar_type_;
+};
+
+class IDVarPartNode : public Node {
+ public:
+  enum class GrammarType {
+    _ID,          //id_varpart → .id 
+    EXP_LIST,     //id_varpart → [ expression_list ]
+  };
+  void TransCode() override;
+ private:
+  GrammarType grammar_type_;
+};
+
+class CaseBodyNode : public Node {
+ public:
+  enum class GrammarType {
+    BRANCH_LIST,   //case_body → branch_list 
+    EPSILON,       //case_body → EPSILON
+  };
+  void TransCode() override;
+  BranchListNode* GetBranchList() {
+    return child_list_[0]->ReinterpretCast<BranchListNode>();
+  }
+ private:
+  GrammarType grammar_type_;
+};
+
+class BranchListNode : public Node {
+ public:
+  enum class GrammarType {
+    SINGLE_BRAN,   //branchlist → branch
+    MULTIPLE_BRAN, //branchlist → branchlist branch
+  };
+  void TransCode() override;
+ private:
+  GrammarType grammar_type_;
+};
+
+class BranchNode : public Node {
+ //branch → const_list : statement
+ public:
+  void TransCode() override;
+ private:
+};
+
+class ConstListNode : public Node {
+ public:
+  enum class GrammarType {
+    SINGLE_CON,   //constlist → const_variable
+    MULTIPLE_CON, //constlist → constlist , const_variable
+  };
+  std::vector<Node*>* Consts() { return &child_list_; }
+ private:
+  GrammarType grammar_type_;
+};
+
+class UpdownNode : public Node {
+ //updown → to | downto
+ public:
+  void TransCode() override;
+  bool IsUpdown();
+ private:
+  bool is_updown_;
+};
+
+class ProcedureCallNode : public Node {
+ public:
+  enum class GrammarType {
+    ID,             //procedure_call → id
+    ID_EXP_LIST,    //procedure_call → id ( expression_list )
+  };
+  void TransCode() override;
+ private:
+  GrammarType grammar_type_;
+};
+
+
+class ElseNode : public Node {
+ public:
+  enum class GrammarType {
+    EPSILON,          //else_part → EPSILON
+    ELSE_STATEMENT,   //else_part → else statement 
+  };
+  void TransCode() override;
+  StatementNode* GetStatement() {
+    return child_list_[0]->ReinterpretCast<StatementNode>();
+  }
+ private:
+  GrammarType grammar_type_;
+};
+
+class ExpressionListNode: public Node {
+ public:
+  enum class GrammarType {
+    EXP,              //expression_list → expression
+    EXP_LIST_EXP,     //expression_list → expression_list , expression
+  };
+  void TransCode() override;
+ private:
+  GrammarType grammar_type_;
+};
+
+
+class ExpressionNode : public Node {
+ public:
+  enum class GrammarType {
+    S_EXP,              //expression → simple_expression 
+    S_EXP_ADDOP_TERM,   //expression → simple_expression relop simple_expression 
+  };
+  void TransCode() override;
+ private:
+  GrammarType grammar_type_;
+};
+
+
+class SimpleExpressionNode : public Node {
+ public:
+  enum class GrammarType {
+    TERM,               //simple_expression → term
+    POSI_TERM,          //simple_expression → +term
+    NEGI_TERM,          //simple_expression → -term
+    S_EXP_ADDOP_TERM,   //simple_expression → simple_expression addop term 
+  };
+  void TransCode() override;
+ private:
+  GrammarType grammar_type_;
+};
+
+
+class TermNode : public Node {
+ public:
+  enum class GrammarType {
+    FACTOR,               //term → factor
+    TERM_MULOP_FACTOR,    //term → term mulop factor
+  };
+  void TransCode() override;
+ private:
+  GrammarType grammar_type_;
+};
+
+
+class FactorNode : public Node {
+ public:
+  enum class GrammarType {
+    UCONST_VAR,     //factor → unsigned_const_variable  
+    VARIABLE,       //factor → variable
+    EXP,            //factor → ( expression )  
+    ID_EXP_LIST,    //factor → id ( expression_list ) 
+    NOT,            //factor → not factor 
+  };
+  void TransCode() override;
+ private:
+  GrammarType grammar_type_;
+};
+
+class UnsignConstVarNode : public Node {
+ public:
+  void TransCode() override;
+  private:
+  
+};
+
+} // namespace ast
+
+
+
+
+#endif
---
+++ src/ast.cc
@@ -0,0 +1,455 @@
+#include "ast.h"
+#include <iostream>
+
+using namespace std;
+
+namespace ast {
+
+Node::Node() : parent_(nullptr) {
+
+}
+
+Node::~Node() {
+
+}
+
+Node* Node::parent(){
+  return parent_;
+}
+
+std::vector<Node*> Node::child_list() {
+  return child_list_;
+}
+
+void Node::set_parent(Node* parent) {
+  parent_ = parent;
+}
+
+void Node::append_child(Node* child) {
+  child_list_.emplace_back(child);
+}
+
+void Node::TransCodeAt(int pos) {
+  return child_list_[pos]->TransCode();
+}
+
+
+//////////////////////////////
+
+void ProgramNode::TransCode(){
+  TransCodeAt(0);
+  TransCodeAt(1);
+}
+
+void ProgramHeadNode::TransCode(){
+  OUT("// ")
+  TransCodeAt(0);
+  OUT("\n")
+}
+
+
+void ProgramBodyNode::TransCode(){
+  TransCodeAt(0);   // const decl
+  TransCodeAt(1);   // type decl // TODO
+  TransCodeAt(2);   // var decl
+  TransCodeAt(3);   // sub prog decl
+  OUT("int main(int argc, char** argv) {\n")
+  OUT("if (argc >= 1) freopen(argv[1], \"r\", stdin);\n")
+  OUT("if (argc >= 2) freopen(argv[2], \"w\", stdout);\n")
+  TransCodeAt(4);   // comp stat
+  OUT("}\n")
+}
+
+void IdListNode::TransCode() {
+  TransCodeAt(0);
+  if (grammar_type_ == GrammarType::MULTIPLE_ID) {
+    OUT(", ")
+    TransCodeAt(1);
+  }
+}
+
+
+void ConstDeclarationsNode::TransCode() {
+  if (grammar_type_ == GrammarType::CONST_DECLARATION) {
+    TransCodeAt(0);
+  }
+}
+
+void ConstDeclarationNode::TransCode(){
+  if (grammar_type_ == GrammarType::DECLARATION) {
+    TransCodeAt(0);
+    OUT(";\nconst ")
+    TransCodeAt(1);
+    OUT(" = ")
+    TransCodeAt(2);
+    OUT(";\n")
+  } else if (grammar_type_ == GrammarType::VALUE) {
+    OUT(";\nconst ")
+    TransCodeAt(0);
+    OUT(" = ")
+    TransCodeAt(1);
+    OUT(";\n")
+  }
+}
+
+void VariableDeclarationsNode::TransCode() {
+  if (grammar_type_ == GrammarType::VAR_DECLARATION) {
+    TransCodeAt(0);
+  }
+}
+
+void VariableDeclarationNode::TransCode() {
+  for (int idx = 0; idx < child_list_.size(); idx += 2) {
+    auto id_list_node = child_list_[idx]->ReinterpretCast<IdListNode>();
+    auto id_list = *id_list_node->IdList();
+    auto type_node = child_list_[idx + 1]->ReinterpretCast<TypeNode>();
+    auto basic_type = type_node->get_basic_type();
+    for (auto id_node : id_list) {
+      basic_type->TransCode();
+      OUT(" ")
+      id_node->TransCode();
+      if (type_node->GetGrammarType() == TypeNode::GrammarType::ARRAY) {
+        type_node->PeriodsTransCode();
+      }
+      OUT(";\n")
+    }
+  }
+}
+
+void TypeDeclarations::TransCode() {
+  if(grammar_type_ == GrammarType::TYPE_DECLARATION) {
+    TransCodeAt(0);
+  }
+}
+
+void TypeDeclaration::TransCode() {
+  for (int idx = 0; idx < child_list_.size(); idx += 2) {
+    child_list_[idx]->TransCode();
+    auto type_node = child_list_[idx + 1]->ReinterpretCast<TypeNode>();
+    auto basic_type = type_node->get_basic_type();
+    OUT("typedef ")
+    basic_type->TransCode();
+    OUT(" ")
+    type_node->TransCode();
+    OUT(";\n")
+  }
+}
+
+void TypeNode::TransCode() {
+  switch (grammar_type_) {
+    case GrammarType::BASIC_TYPE:
+      TransCodeAt(0);
+      break;
+    case GrammarType::ARRAY: // upper funciton will solve this
+      break;
+    case GrammarType::RECORD_TYPE:
+      OUT("struct {\n")
+      TransCodeAt(0);
+      OUT("}")
+      break;
+  }
+}
+
+void TypeNode::PeriodsTransCode() {
+  if (grammar_type_ == GrammarType::ARRAY) {
+    child_list_[0]->TransCode();
+    child_list_[1]->ReinterpretCast<TypeNode>()->PeriodsTransCode();
+  }
+}
+
+Node* TypeNode::get_basic_type() {
+  if (grammar_type_ != GrammarType::ARRAY) {
+    return this;
+  } else {
+    return child_list_[1]->ReinterpretCast<TypeNode>()->get_basic_type();
+  }
+}
+
+void RecordBodyNode::TransCode() {
+  if (grammar_type_ == GrammarType::VAR_DECLARATION) {
+    TransCodeAt(0);
+  }
+}
+
+void PeriodsNode::TransCode() {
+  for (auto child : child_list_) {
+    child->TransCode();
+  }
+}
+
+void PeriodNode::TransCode() {
+  OUT("[%d]", len_)
+}
+
+void SubprogramDeclarationsNode::TransCode() {
+  for (auto child : child_list_) {
+    child->TransCode();
+  }
+}
+
+void SubprogramDeclarationNode::TransCode() {
+
+}
+
+void SubprogramHeadNode::TransCode() {
+
+}
+
+void FormalParamNode::TransCode() {
+
+}
+
+void ParamListsNode::TransCode() {
+
+}
+
+void ParamListNode::TransCode() {
+
+}
+
+void VarParamNode::TransCode() {
+
+}
+
+void ValueParamNode::TransCode() {
+
+}
+
+void CompoundStatementNode::TransCode() {
+  OUT("{\n")
+  TransCodeAt(0);
+  OUT("}\n")
+}
+
+void StatementListNode::TransCode() {
+  for (auto child : child_list_) {
+    child->TransCode();
+  }
+}
+
+void StatementNode::TransCode() {
+  switch (grammar_type_) {
+    case GrammarType::EPSILON:
+      return;
+    case GrammarType::VAR_ASSIGN_OP_EXP:
+      TransCodeAt(0);
+      OUT(" = ")
+      TransCodeAt(1);
+      OUT(";\n")
+      break;
+    case GrammarType::FUNC_ASSIGN_OP_EXP:
+      TransCodeAt(0);
+      OUT(" = ")
+      TransCodeAt(1);
+      OUT(";\n");      
+      break;
+    case GrammarType::PROCEDURE_CALL:
+      TransCodeAt(0);
+      break;
+    case GrammarType::COMPOUND_STATEMENT:
+      TransCodeAt(0);
+      break;
+    case GrammarType::IF_STATEMENT:
+      OUT("if (")
+      TransCodeAt(0);
+      OUT(") {\n")
+      TransCodeAt(1);
+      OUT("}\n")
+      TransCodeAt(2);
+      break;
+    case GrammarType::FOR_STATEMENT:
+      auto updown_node = child_list_[2]->ReinterpretCast<UpdownNode>();
+      bool reverse = updown_node->IsUpdown();
+      OUT("for (")
+      TransCodeAt(0);
+      OUT("=")
+      TransCodeAt(1);
+      OUT(";")
+      TransCodeAt(0);
+      OUT(reverse ? ">=" : "<=")
+      TransCodeAt(3);
+      OUT(";")
+      TransCodeAt(0);
+      OUT(reverse ? "--" : "++")
+      OUT(") {\n")
+      for (int i = 4; i < child_list_.size(); i++)
+        TransCodeAt(i);
+      OUT("}\n")
+      break;
+    case GrammarType::READ_STATEMENT:
+    // TODO
+      break;
+    case GrammarType::WRITE_STATEMENT:
+    // TODO
+      break;
+    case GrammarType::CASE_STATEMET:
+      OUT("switch (")
+      TransCodeAt(0);
+      OUT(") {\n")
+      TransCodeAt(1);
+      OUT("}\n")
+      break;
+    case GrammarType::WHILE_STATEMENT:
+      OUT("while (")
+      TransCodeAt(0);
+      OUT(") {\n")
+      TransCodeAt(1);
+      OUT("}\n")
+      break;
+    case GrammarType::REPEAT_STATEMENT:
+      OUT("do {\n")
+      TransCodeAt(0);
+      OUT("} while (")
+      TransCodeAt(1);
+      OUT(");\n")
+      break;
+  }
+}
+
+void VariableListNode::TransCode() {
+  TransCodeAt(0);
+  if(grammar_type_ == GrammarType::VARIABLE_LIST_VARIABLE) {
+    OUT(",")
+    TransCodeAt(1);
+  }
+}
+
+void VariableNode::TransCode(){
+  for(auto& child : child_list_) {
+    child->TransCode();
+  }
+}
+
+void IDVarPartsNode::TransCode() {
+  for(auto& child : child_list_) {
+    child->TransCode();
+  }
+}
+
+void IDVarPartNode::TransCode() {
+  if (grammar_type_ == GrammarType::_ID) {
+    OUT(".")
+    TransCodeAt(0);
+  } else if (grammar_type_ == GrammarType::EXP_LIST) {
+    OUT("[")
+    TransCodeAt(0);
+    OUT("]")
+  }
+}
+
+void CaseBodyNode::TransCode() {
+  switch (grammar_type_) {
+    case GrammarType::EPSILON:
+      return;
+    case GrammarType::BRANCH_LIST:
+      GetBranchList()->TransCode();
+  }
+}
+
+void BranchListNode::TransCode() {
+  for (auto child : child_list_)
+    child->TransCode();
+}
+
+void BranchNode::TransCode() {
+  for (int id = 0; id < child_list_.size(); id += 2) {
+    auto const_list = child_list_[id]->ReinterpretCast<ConstListNode>();
+    auto statement = child_list_[id + 1];
+    auto const_vars = const_list->Consts();
+    for (auto const_var : *const_vars) {
+      OUT("case ")
+      const_var->TransCode();
+      OUT(":\n")
+      statement->TransCode();
+    }
+  }
+}
+
+// const_list no need
+
+void UpdownNode::TransCode() {
+  TransCodeAt(0);
+}
+
+void ProcedureCallNode::TransCode() {
+  TransCodeAt(0);
+  if (grammar_type_ == GrammarType::ID) {
+    OUT("();")
+  } else if(grammar_type_ == GrammarType::ID_EXP_LIST) {
+    OUT("(")
+    TransCodeAt(1);
+    OUT(");")
+  }
+}
+
+void ElseNode::TransCode() {
+  switch (grammar_type_) {
+    case GrammarType::EPSILON:
+      return;
+    case GrammarType::ELSE_STATEMENT:
+      OUT("else {\n")
+      GetStatement()->TransCode();
+      OUT("}\n")
+  }
+}
+
+void ExpressionListNode::TransCode() {
+  TransCodeAt(0);
+  OUT(",")
+  TransCodeAt(1);
+}
+
+void ExpressionNode::TransCode() {
+  for(auto& child : child_list_){
+    child->TransCode();
+  }
+}
+
+void SimpleExpressionNode::TransCode() {
+  for (auto& child : child_list_) {
+    child->TransCode();
+  }
+}
+
+void TermNode::TransCode() {
+  for (auto& child : child_list_) {
+    child->TransCode();
+  }
+}
+
+void FactorNode::TransCode() {
+
+  switch (grammar_type_) {
+    case GrammarType::UCONST_VAR :
+    case GrammarType::VARIABLE :
+      TransCodeAt(0);
+      break;
+
+    case GrammarType::EXP : 
+      OUT("(")
+      TransCodeAt(0);
+      OUT(")")
+      break;
+      
+    case GrammarType::ID_EXP_LIST : 
+      TransCodeAt(0);
+      OUT("(")
+      TransCodeAt(1);
+      OUT(")")
+      break;
+
+    case GrammarType::NOT : 
+      OUT("!")
+      TransCodeAt(0);
+      break;
+      
+    default :
+      break;
+  }
+}
+
+void UnsignConstVarNode::TransCode() {
+  TransCodeAt(0);
+}
+
+}  // namespace ast
