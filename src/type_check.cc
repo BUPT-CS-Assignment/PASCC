c4e82fd28725bd84aee696f77f7734c3d8fc8a07
Kqramazov <yurisvarozhich@gmail.com>
2023-03-29 20:34:51 +0800

    access_check

--- src/type_check.cc
+++ src/type_check.cc
@@ -1,6 +1,7 @@
 #include "type.h"
 #include <unordered_set>
 #include <vector>
+#include <string>
 
 using pascal_type::TYPE_INT;
 using pascal_type::TYPE_CHAR;
@@ -9,6 +10,14 @@ using pascal_type::TYPE_REAL;
 
 #define IN_BASIC_TYPE(a)                                                       \
   (a == INT) || (a == CHAR) || (a == BOOL) || (a == _recalloc)
+
+
+// 这一段结构体声明之后应该放在别的地方
+typedef struct VarParts{
+    bool flag;//0 for Record.1 for array
+    std::vector<pascal_type::TypeTemplate*> subscript;
+    std::string name;
+ }VarParts;
 
 // 基本与Record类型的类型检查
 bool CheckType(pascal_type::TypeTemplate *a, pascal_type::TypeTemplate *b) {
@@ -59,4 +68,54 @@ bool AssertArrayAccess(pascal_type::ArrayType a,
       // RaiseError
     }
 */
+}
+
+
+
+// unfinished
+bool LegalAccess(pascal_type::TypeTemplate *a,
+                        std::vector<VarParts> var,
+                        pascal_type::TypeTemplate **result){
+    // 
+    auto tmp_a = a;
+    bool is_in_array=0;
+    std::vector<pascal_type::TypeTemplate*> cur_subscript;
+    for(int i=0;i<var.size();i++){
+        if(is_in_array==0){
+            if(var[i].flag==0){
+                // 从符号表中由名字取出属性
+                // tmp_a = SymbleTable.find(tmp_a, var[i].name);
+            }else{
+                pascal_type::TypeTemplate *access_result;
+                // 需要进行类型转换，此处写得很强行
+                if(AssertArrayAccess(*(pascal_type::ArrayType*)tmp_a, var[i].subscript,&access_result)){
+                    tmp_a = access_result;
+                }else{
+                    cur_subscript.assign(var[i].subscript.begin(), var[i].subscript.end());
+                    is_in_array=1;
+                }
+            }
+        }else{
+            if(var[i].flag==0){
+                // error(TyoeError);
+                return false;
+            }else{
+                pascal_type::TypeTemplate *access_result;
+                cur_subscript.assign(var[i].subscript.begin(), var[i].subscript.end());
+                if(AssertArrayAccess(*(pascal_type::ArrayType*)tmp_a,cur_subscript,&access_result)){
+                    tmp_a = access_result;
+                    is_in_array=0;
+                    cur_subscript.clear();
+                }
+            }
+        }
+    }
+    if(is_in_array=0){
+        *result=tmp_a;
+        return true;
+    }else{
+        *result=nullptr;
+        return false;
+    }
+
 }
