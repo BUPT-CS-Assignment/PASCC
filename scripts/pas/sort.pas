cde26bfded1fe1681b0a3bdc1aa1f33d6ee2db4f
Jianxff <3109441270@qq.com>
2023-04-25 00:47:11 +0800

    [U] update scripts

---
+++ scripts/pas/sort.pas
@@ -0,0 +1,52 @@
+program TestQsort(input,output);
+
+var
+  n: integer;
+  a: array[0..10000] of integer;
+
+procedure quickSort(l, r: integer);
+var
+  i, j, pivot: integer;
+begin
+  if l < r then
+  begin
+    i := l;
+    j := r;
+    pivot := a[i];
+    while i < j do
+    begin
+      while (i < j) and (a[j] >= pivot) do
+        j := j - 1;
+      a[i] := a[j];
+      while (i < j) and (a[i] <= pivot) do
+        i := i + 1;
+      a[j] := a[i];
+    end;
+    a[i] := pivot;
+    quickSort(l, i - 1);
+    quickSort(i + 1, r);
+  end;
+end;
+
+procedure readData;
+var
+  i: integer;
+begin
+  for i := 0 to n - 1 do
+
+end;
+
+procedure printData;
+var
+  i: integer;
+begin
+  for i := 0 to n - 1 do
+
+end;
+
+begin
+  readData;
+  printData;
+  quickSort(0, n - 1);
+  printData;
+end.
---
+++ scripts/pas/test.pas
@@ -0,0 +1,8 @@
+program testadd(input,output);
+
+var
+  a, b, c: integer;
+
+begin
+  c := a - b;
+end.
