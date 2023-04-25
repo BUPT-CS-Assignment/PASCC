a83bfc2ad2658e53eea8048adc4eded414628c02
蓝桥 <381602449@qq.com>
2023-04-25 10:58:10 +0800

    [A] Add test module

---
+++ test/test_sample.py
@@ -0,0 +1,30 @@
+import pytest
+import subprocess
+
+def run_program(input_file):
+    input_file_c = input_file.replace('.txt', '.c')
+    subprocess.run(['../build/Release/Actions_Test.exe', '-I', input_file, input_file_c], check=True)
+    
+    process = subprocess.Popen(['gcc', input_file_c], stdout=subprocess.PIPE)
+    output, error = process.communicate()
+    return output.decode('utf-8').strip()
+
+def read_expected_output(output_file):
+    
+    with open(output_file, 'r') as f:
+        expected_output = f.read().strip()
+    return expected_output
+
+def check_program(input_file, expected_output):
+
+    output = run_program(input_file)
+    expected_output = read_expected_output(expected_output)
+    assert output == expected_output
+
+@pytest.mark.parametrize("input_file, expected_output", [
+    ("input/input01.txt", "output/output01.txt"),
+    ("input/input02.txt", "output/output02.txt")
+])
+
+def test_program_with_multiple_cases(input_file, expected_output):
+    check_program(input_file, expected_output) 
