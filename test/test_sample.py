4745754bcf1b7ca09fb28deff21f8329fa8dbc15
蓝桥 <381602449@qq.com>
2023-04-26 01:38:23 +0800

    [U] Update test_module

--- test/test_sample.py
+++ test/test_sample.py
@@ -1,5 +1,6 @@
-import pytest
 import subprocess
+
+test_case_num = 9
 
 def run_program(input_file):
     input_file_c = input_file.replace('.txt', '.c')
@@ -23,10 +24,12 @@ def check_program(input_file, expected_output):
     expected_output = read_expected_output(expected_output)
     assert output == expected_output
 
-@pytest.mark.parametrize("input_file, expected_output", [
-    ("input/input01.txt", "output/output01.txt"),
-    ("input/input02.txt", "output/output02.txt")
-])
-
-def test_program_with_multiple_cases(input_file, expected_output):
-    check_program(input_file, expected_output) 
+def test_program_with_multiple_cases():
+    test_cases = []
+    for i in range(1, test_case_num + 1):
+        test_input = f"input/input{i:02d}.txt"
+        test_output = f"output/output{i:02d}.txt"
+        test_cases.append((test_input, test_output)) 
+    
+    for input_file, expected_output in test_cases:
+        check_program(input_file, expected_output)  
