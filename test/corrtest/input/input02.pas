5d48635b0923c67ad6f491ea76fc13942040a54a
Jianxff <3109441270@qq.com>
2023-05-06 00:44:13 +0800

    [F] Fix array visit

--- test/corrtest/input/input02.pas
+++ test/corrtest/input/input02.pas
@@ -3,7 +3,8 @@ var
   a, b: array [1..10] of array ['a'..'z'] of record x: integer; y:char end;
 begin
   a[2]['a'].x := 10;
-  b[1]['a'] := a[2]['b'];
+  b := a;
+  b[3] := a[2];
   b[5]['b'].y := 'c';
   writeln(b[2]['a'].x);
   writeln(b[5]['b'].y);
