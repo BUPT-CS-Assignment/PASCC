3a78cea44fafbad7b727631f396db3e9a10e71c3
lanqiaoZzz <381602449@qq.com>
2023-05-06 14:38:53 +0800

    [U] Update test cases

--- test/corrtest/input/input04.pas
+++ test/corrtest/input/input04.pas
@@ -1,7 +1,49 @@
-program TestIf;
+program TestProcAndFunc;
+
 var
-  a: integer;
+  a, b: integer;
+
+procedure testPro1;
 begin
-  a := 10;
-  if a > 0 then write(a);
+  writeln(2023);
+end;
+
+procedure testPro2(n: integer);
+begin
+  writeln(n + 1);
+end;
+
+function testFunc: integer;
+begin
+  writeln(2023);
+end;
+
+function gcd(a, b: integer): integer;
+begin
+  if b = 0 then gcd := a
+  else gcd := gcd(b, a mod b);
+end;
+
+function swap(var x, y: integer): integer;
+var
+  temp: integer;
+begin
+  temp := x;
+  x := y;
+  y := temp;
+end;
+
+begin
+  writeln("procedure-test");
+  testPro1;
+  testPro2(2022);
+  
+  writeln("");
+  writeln("function-test");  
+  testFunc;
+  a := 54;
+  b := 24;
+  writeln(gcd(a, b));
+  swap(a, b);
+  writeln(a, " ", b);
 end.
--- test/corrtest/input/input07.pas
+++
@@ -1,7 +0,0 @@
-program TestChar;
-var
-  c: char;
-begin
-  c := 'A';
-  write(c);
-end.
