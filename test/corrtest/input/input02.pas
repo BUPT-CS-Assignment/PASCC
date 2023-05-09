dcdd07138939e221d488b061656308d40b4000a7
lanqiaoZzz <381602449@qq.com>
2023-05-09 20:07:47 +0800

    [U] Add string constant error

--- test/corrtest/input/input02.pas
+++ test/corrtest/input/input02.pas
@@ -4,7 +4,7 @@ var
   rec2: record x: integer; y: char; end; 
   rec3: record x: record x1: integer; x2: real; end; y: char; end;
 begin
-  { record赋值 }
+  // record 赋值
   rec1.x := 2023;
   rec1.y := 't';
   rec2.x := rec1.x;
@@ -12,7 +12,7 @@ begin
   writeln(rec2.x);
   writeln(rec2.y);
 
-  { record嵌套 }
+  // 嵌套 record 赋值
   rec3.x.x1 := 2023;
   rec3.x.x2 := 3.14;
   rec3.y := 't';
--- test/corrtest/input/input03.pas
+++ test/corrtest/input/input03.pas
@@ -3,24 +3,24 @@ var
   arr1: array[1..3] of integer;  
   arr2, arr3: array [1..10] of array ['a'..'z'] of record x: integer; y: char; end; 
 begin
-  { 一维数组赋值 }
+  // 一维数组赋值
   arr1[1] := 1;
   arr1[2] := 2;
   arr1[3] := 3;
   writeln(arr1[3]);
   
-  { 二维record数组赋值 }
+  // 二维record数组赋值
   arr2[1]['a'].x := 1;
   arr2[1]['a'].y := 'a';
   arr2[2]['b'].x := 2;
   arr3[1]['a'].x := arr2[1]['a'].x;
   writeln(arr3[1]['a'].x);
   
-  { 二维数组行拷贝 }
+  // 二维数组行拷贝
   arr3[2] := arr2[2];
   writeln(arr3[2]['b'].x);
   
-  { 二维数组拷贝 }
+  // 二维数组拷贝
   arr3 := arr2;
   writeln(arr3[1]['a'].y);
 end.
