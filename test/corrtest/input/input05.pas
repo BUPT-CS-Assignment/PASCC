70e1f32683acc837efb81c759da9ae985c2cf681
lanqiaoZzz <381602449@qq.com>
2023-05-10 00:21:51 +0800

    [U] Update test cases

--- test/corrtest/input/input05.pas
+++ test/corrtest/input/input05.pas
@@ -3,28 +3,19 @@ program TestProcAndFunc;
 var
   a, b: integer;
 
+// 无参过程
 procedure testPro1;
 begin
   writeln(2023);
 end;
 
+// 有参（传值）过程
 procedure testPro2(n: integer);
 begin
   writeln(n + 1);
 end;
 
-function testFunc: integer;
-begin
-  writeln(2023);
-  testFunc := 54;
-end;
-
-function gcd(a, b: integer): integer;
-begin
-  if b = 0 then gcd := a
-  else gcd := gcd(b, a mod b);
-end;
-
+// 有参（传引用）过程
 procedure swap(var x, y: integer);
 var
   temp: integer;
@@ -34,6 +25,21 @@ begin
   y := temp;
 end;
 
+// 无参函数
+function testFunc: integer;
+begin
+  writeln('before testFunc := 54');
+  testFunc := 54;
+  writeln('after testFunc := 54');
+end;
+
+// 有参（传值）递归函数
+function gcd(a, b: integer): integer;
+begin
+  if b = 0 then gcd := a
+  else gcd := gcd(b, a mod b);
+end;
+
 begin
   writeln('procedure-test');
   testPro1;
@@ -41,9 +47,9 @@ begin
   
   writeln();
   writeln('function-test');  
-  a := testFunc;
+  a := testFunc();
   b := 24;
-  writeln(gcd(a, b));
+  writeln(gcd(a, b) + 1);
   swap(a, b);
   writeln(a, ' ', b);
 end.
